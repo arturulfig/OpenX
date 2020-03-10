f98398612ed950cbc715a331a07fb3c993b0b3d0
Artur Ulfig <artur.ulfig2@gmail.com>
2020-03-10 09:11:51 +0100

    Second task - python

--- Zad_2/main.py
+++ Zad_2/main.py
@@ -1,6 +1,9 @@
 import json
 import requests
 import pandas as pd
+import numpy as np 
+from math import *
+from pandas import json_normalize
 
 def getJsons(url):
     try:
@@ -43,6 +46,32 @@ def uniquePosts(postsJson):
     return unique
 
 
+##Haversine formula
+def haversineForm(lat1, lon1, lat2, lon2):
+    earthRadius = 6371.0
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lon2, lat2 = map(np.radians, [lon2, lat2])
+
+    #difference between two latlons
+    lat3 = lat2 - lat1
+    lon3 = lon2 - lon1
+    a = np.sin(lat3 / 2.0) ** 2 + cos(lat1) * np.cos(lat2) * np.sin(lon3 / 2.0) ** 2
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    haversine = earthRadius * c
+    return haversine
+
+def nearest(userDataFrame):
+    userDataFrame = userDataFrame.filter(['id', 'username', 'address.geo.lat', 'address.geo.lng'])
+    userDataFrame['address.geo.lat'] = pd.to_numeric(userDataFrame['address.geo.lat'])
+    userDataFrame['address.geo.lng'] = pd.to_numeric(userDataFrame['address.geo.lng'])
+    users = {}
+    for i in range(len(userDataFrame)):
+        distance = haversineForm(userDataFrame.iat[i, 2], userDataFrame.iat[i, 3],
+                                           userDataFrame['address.geo.lat'], userDataFrame['address.geo.lng'])
+        minDistance = distance[distance > 0].idxmin()
+        users.update({userDataFrame.iat[i, 1]: userDataFrame.iat[minDistance, 1]})
+    return users
 
 def main():
     responseFromUsers = getJsons("https://jsonplaceholder.typicode.com/users")
@@ -60,5 +89,10 @@ def main():
     print(countedPosts)
 
     print(uniquePosts(postsJson))
+
+    nearestUsers = nearest(json_normalize(usersJson))
+    for i, j in nearestUsers.items():
+        print("Najblizszym sasiadem {0} jest {1}".format(i,j))
+
 if __name__ == '__main__':
     main()
